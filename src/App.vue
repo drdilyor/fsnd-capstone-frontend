026c27101d3e16f0113452ad9d6c111aff9e9c4b
drdilyor <drdilyor@outlook.com>
2021-04-01 12:30:57 +0500

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <app-header/>
     <router-view class="flex-grow-1"/>
+    <app-footer/>
   </div>
 </template>
 
 <script>
 import '@/assets/lib/bootstrap.min.css'
+import '@/assets/scss/styles.scss'
 export default {
   components: {
     AppHeader: require('@/components/AppHeader.vue').default,
+    AppFooter: require('@/components/AppFooter.vue').default,
   }
 }
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="bg-light pt-4">
+    <div class="container">
+      <p>Casting Agency &copy; Dilyorbek Valijonov (drdilyor)</p>
+      <p>
+        <a href="https://t.me/drdilyor">Telegram</a>
+        <span class="spacer"></span>
+        <a href="https://github.com/drdilyor">GitHub</a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style>
+footer > .container {
+  text-align: center;
+}
+</style>
