b04b447b960a9a6df22e0f805eba31bed924aa42
drdilyor <drdilyor@outlook.com>
2021-04-01 10:38:20 +0500

    emptied and inited

---
+++ src/views/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <h1>404</h1>
+    <p>Page you are requesting was not found</p>
+    <p><router-link to="/">Main page</router-link></p>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <p>Home page</p>
+    <div class="alert alert-success">HEllo world</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
