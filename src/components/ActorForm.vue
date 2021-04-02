a1c3a1d2667f0d9e46b7de5b3ac529e653697d37
drdilyor <drdilyor@outlook.com>
2021-04-02 21:06:58 +0500

    Move: Actor detail content to ActorForm

---
+++ src/components/ActorForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent="$emit('submit')">
+    <label class="form-label" for="actorName">Name:</label>
+    <input class="form-control" id="actorName" type="text" v-model="actor.name">
+    <div class="mb-3"/>
+    <label class="form-label" for="actorAge">Age:</label>
+    <input class="form-control" id="actorMovie" type="number" v-model="actor.age">
+    <div class="mb-3"/>
+    <label class="form-label" for="actorMovie">Movie id:</label>
+    <input class="form-control" id="actorMovie" type="number" v-model="actor.movie_id">
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    actor: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
--- src/views/ActorDetail.vue
+++ src/views/ActorDetail.vue
@@ -1,19 +1,7 @@
 <template>
   <!-- This component will be used as a modal nested view -->
     <modal :title="actor === null ? 'Loading...' : actor.name" @close="close">
-      <div v-if="actor !== null">
-        <p>
-          <span class="text-primary">Age</span>: {{ actor.age }}
-        </p>
-        <p>
-          <span class="text-primary">Gender</span>: {{ ['Man', 'Woman'][actor.gender] }}
-        </p>
-        <p>
-          <span class="text-primary">Movie id</span>: 
-          <router-link v-if="actor.movie_id" to="'/movies/'+actor.movie_id">{{ actor.movie_id }}</router-link>
-          <span v-else>Not assigned to a movie yet</span>
-        </p>
-      </div>
+      <actor-form v-if="actor !== null" :actor="actor"/>
       <template #footer>
         <button class="btn btn-primary" type="button" @click="close">Close</button>
       </template>
@@ -24,6 +12,7 @@
 export default {
   components: {
     Modal: require('@/components/ui/Modal.vue').default,
+    ActorForm: require('@/components/ActorForm.vue').default,
   },
   data() { return {
     actor: null,
